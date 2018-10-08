84ab60f08adf199fd33d52d570e9787ac9ec0486
aditya <agaurav51@gmail.com>
2018-10-08 14:08:13 +0530

    Added configs and formatting

--- deeppress/predict.py
+++ deeppress/predict.py
@@ -3,12 +3,23 @@ from keras.models import load_model
 import numpy as np
 import cv2
 import urllib.request
-base_url = "http://192.168.0.12:8000"
+from deeppress.config import config
+import os
+base_url = config.WP_BASE_URL
+
+
 def model_load(filename):
-    model = load_model('{}/model/{}.h5'.format(filename, filename))
+    """This function loads the .h5 model file for the filename argument"""
+
+    model = load_model(os.path.join(config.TRAINED_MODELS_DATA, filename) + '/{}.h5'.format(filename))
     return model
 
+
 def get_image(im_url):
+    """This function fetches the image for the given path so that it can be 
+    classified
+    """
+
     url = base_url + im_url
     url_response = urllib.request.urlopen(url)
     img_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
@@ -17,10 +28,15 @@ def get_image(im_url):
     img = np.reshape(img, (1,100,100,3))
     return img
 
+
 def get_labels(filename):
-    path = '/home/aditya/{}/model/labels.txt'.format(filename)
-    with open(path) as json_file:  
-         data = json.load(json_file)
+    """This function parses the label file (.txt) as a json file to load the 
+    categories for which the model has been trained
+    """
+    
+    path = os.path.join(config.TRAINED_MODELS_DATA, filename) + '/labels.txt'
+    with open(path) as json_file:
+        data = json.load(json_file)
     labels = {}
     names = {}
     for p in data['category']:
@@ -28,7 +44,13 @@ def get_labels(filename):
         names[p['id']]=p['name']
     return labels, names
 
+
 def predict_class(img, model, labels, names):
+    """This function finally predicts the category for the given image and 
+    classifies it while retturning the class ID, class name and the confidence
+    score
+    """
+
     p = model.predict(img)
     pred = np.argmax(p, axis=1)
     predicted_id = labels[pred]
