33ef27fec4403daf97a5716515b2bb0190487186
aditya <agaurav51@gmail.com>
2018-10-11 17:37:27 +0530

    Added Model checkpoints

--- deeppress/train.py
+++ deeppress/train.py
@@ -1,5 +1,5 @@
 from keras import backend as K
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
@@ -16,13 +16,13 @@ from deeppress.config import config
 configuration = tf.ConfigProto( device_count = {'GPU': 1} ) 
 sess = tf.Session(config=configuration) 
 keras.backend.set_session(sess)
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 import logging
 
 _logger = logging.getLogger('backend.train')
 
 batch_size = 16 #constrained to GPU capacity
-epochs = 100
+epochs = 20
 input_size=[100,100]
 
 
@@ -52,7 +52,6 @@ def create_gens(train_path, gen):
         class_indices = train_generator.class_indices
     except FileNotFoundError:
         _logger.error("data generators invalid")
-        print("Error : Unable to create data genrators")
         train_generator = False
         test_generator = False
         image_files = False
@@ -60,7 +59,7 @@ def create_gens(train_path, gen):
     return train_generator, test_generator, image_files, class_indices
 
 
-def start_training(model, train_generator, test_generator, image_files, filename, job):
+def start_training(model, train_generator, test_generator, image_files, filename, job, status):
     """This function finally trains the classifier to classify the images according
     to the category labels found in the dataset. After training is complete the 
     trained model (.h5 file) is saved in the '/<filename>/model/' local directory
@@ -69,27 +68,64 @@ def start_training(model, train_generator, test_generator, image_files, filename
     """
 
     _logger.debug("Start Training")
-    callbacks = [EarlyStopping(
-        monitor='val_loss', 
-        min_delta=0, patience=5, 
-        verbose=0, 
-        mode='auto', 
-        baseline=None)]
-    state = api.update_job_state(job, 'training', 'Start training for {} epochs'.format(epochs))
-    r = model.fit_generator(
-        train_generator,
-        validation_data=test_generator,
-        epochs=epochs,
-        callbacks = callbacks,
-        steps_per_epoch= (0.8 * len(image_files)) // batch_size,
-        validation_steps=(0.2 * len(image_files)) // batch_size,
-    )  
+    flag = 1
     path = os.path.join(config.TRAINED_MODELS_DATA, filename)
-    os.makedirs(path)
-    model_file = os.path.abspath(path + ('{}.h5'.format(filename)))
-    model.save(model_file)
-    #print("Trained model saved at {}".format(model_file))
-    return r.history['acc'][-1], r.history['loss'][-1], r.history['val_acc'][-1], r.history['val_loss'][-1]
+    if status == "Added":
+        os.makedirs(path)
+        callbacks = [ModelCheckpoint(
+            filepath = os.path.join(path, '{}tmp.h5'.format(filename)), 
+            monitor='val_loss', 
+            verbose=0, 
+            save_best_only=False, 
+            save_weights_only=False, 
+            mode='auto', 
+            period=1)]
+        state = api.update_job_state(job, 'training', 'Start training for {} epochs'.format(epochs))
+        try:
+            r = model.fit_generator(
+                train_generator,
+                validation_data=test_generator,
+                epochs=epochs,
+                callbacks = callbacks,
+                steps_per_epoch= (0.8 * len(image_files)) // batch_size,
+                validation_steps=(0.2 * len(image_files)) // batch_size,
+            )
+        except Exception:
+            return False, False, False, False, False
+        if len(r.history['acc']) < epochs:
+            flag = 0
+            return flag, r.history['acc'][-1], r.history['loss'][-1], r.history['val_acc'][-1], r.history['val_loss'][-1]
+        else:
+            model_file = os.path.join(path + ('{}.h5'.format(filename)))
+            model.save(model_file)
+            return flag, r.history['acc'][-1], r.history['loss'][-1], r.history['val_acc'][-1], r.history['val_loss'][-1]
+    elif status == "incomplete":
+        model = load_model(os.path.join(path, '{}tmp.h5'.format(filename)))
+        callbacks = [ModelCheckpoint(
+            filepath = os.path.join(path, '{}tmp.h5'.format(filename)), 
+            monitor='val_loss', 
+            verbose=0, 
+            save_best_only=False, 
+            save_weights_only=False, 
+            mode='auto', 
+            period=1)]
+        state = api.update_job_state(job, 'training', 'Start training for {} epochs'.format(epochs))
+        r = model.fit_generator(
+            train_generator,
+            validation_data=test_generator,
+            epochs=epochs,
+            callbacks = callbacks,
+            steps_per_epoch= (0.8 * len(image_files)) // batch_size,
+            validation_steps=(0.2 * len(image_files)) // batch_size,
+        )
+        if len(r.history['acc']) < epochs:
+            flag = 0
+            return flag, r.history['acc'][-1], r.history['loss'][-1], r.history['val_acc'][-1], r.history['val_loss'][-1]
+        else:
+            model_file = os.path.join(path + ('{}.h5'.format(filename)))
+            model.save(model_file)
+            return flag, r.history['acc'][-1], r.history['loss'][-1], r.history['val_acc'][-1], r.history['val_loss'][-1]
+
 
 
 def create_labels(cat_dict, filename, class_indices):
